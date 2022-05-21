021ff78b0c6109bcb449dbb588e08c0043aaff1f
leeool <lgonsalez02@gmail.com>
2022-05-20 22:05:18 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int dividendo, resto, divisor;
+
+Console.WriteLine("Máximo Divisor Comum\n");
+
+Console.Write("Digite o 1° número: ");
+dividendo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o 2° número: ");
+divisor = Convert.ToInt32(Console.ReadLine());
+
+do{
+    resto = dividendo % divisor;
+    dividendo = divisor;
+    divisor = resto;
+}
+while (resto != 0);
+
+Console.WriteLine($"MDC(dividendo, divisor) = {dividendo}");
